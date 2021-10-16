eea406ee979d926406ef7e26025ea17ac640e9f1
guayana <isabelaguayanaz@gmail.com>
2021-10-16 13:53:27 -0300

    estilização do banner

---
+++ components/Banner/background.js
@@ -0,0 +1,7 @@
+import { Box, Image, Text, Flex } from '@chakra-ui/react'
+
+export function Background({children}){
+  return(
+    <Image src="/images/bg.jpeg" alt="banner" />
+  )
+}
---
+++ components/Banner/index.js
@@ -0,0 +1,36 @@
+import { Box, Image, Text, Flex } from '@chakra-ui/react'
+
+export default function Banner(){
+  return(
+    <Flex
+      flexDir="row"
+      justifyContent="space-around"
+      h="300"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundImage="/images/bg.jpeg"
+    >
+      <Flex 
+        color="white" 
+        flexDir="column" 
+        justifyContent="center"
+      >
+        <Text fontSize="35" mb="5" fontWeight="400">
+          5 Continentes, 
+          <br />
+          infinitas possibilidades.
+        </Text>
+
+        <Text fontSize="20" fontWeight="300">
+          Chegou a hora de tirar do papel a viagem que você
+          <br />  
+          sempre sonhou.
+        </Text>
+      </Flex>
+
+      <Box py="10">
+        <Image src="/images/airplane.svg"  />
+      </Box>
+    </Flex>
+  )
+}
