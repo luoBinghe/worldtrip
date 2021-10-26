6602e292ea4a6087f39d97f9f711b200bcd22de9
guayana <isabelaguayanaz@gmail.com>
2021-10-26 17:48:33 -0300

    banner

--- pages/continents/north_america.js
+++ pages/continents/north_america.js
@@ -1,9 +1,21 @@
 import Header from "../../components/Header"
+import { Flex, Text } from '@chakra-ui/react'
 
 export default function NorthAmerica(){
     return(
       <>
         <Header inRoute={true} />
+        <Flex
+          w="100%"
+          h="400"
+          backgroundImage="/images/banner.jpeg"
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+          alignItems="end"
+          p="20"
+        >
+          <Text fontWeight="bold" fontSize="35" color="white">America do norte</Text>
+        </Flex>
       </>
     )
 }
